234c355a636e19053f2d353f0a84019d6e722ad3
qcuonghector-boop <qcuong.hector@gmail.com>
2026-03-12 22:23:32 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,5 +1,12 @@
 use master;
 go
+
+if exists (select 1 from sys.databases where name = 'datawarehouse')
+begin
+	alter database set single_user with rollback immediate;
+	drop database datawarehouse
+end;
+go
 create database datawarehouse;
 go
 use datawarehouse;
