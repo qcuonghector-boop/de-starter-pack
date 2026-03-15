5eb93561f38deb0a637d0638c2670d6d550441a7
qcuonghector-boop <qcuong.hector@gmail.com>
2026-03-15 16:13:30 +0700

    Update 3. init_bulk_insert.sql

--- scripts/3. init_bulk_insert.sql
+++ scripts/3. init_bulk_insert.sql
@@ -1,124 +1,123 @@
+create or alter procedure bronze.load_bronze as
+begin
+	
+	declare @start_time datetime;
+	declare @end_time datetime;
 
--- bronze.crm_cust_info -- 
+	set @start_time = getdate();
 
-truncate table bronze.crm_cust_info
+	begin try
+		print 'Loading Bronze Layer';
 
-;
+		print 'Loading CRM Table';
 
-bulk insert bronze.crm_cust_info
-from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-with 
-	(
-	firstrow = 2
-	, fieldterminator = ','
-	, tablock
-	)
+		-- bronze.crm_cust_info -- 
 
-;
-select count(*)
-from bronze.crm_cust_info
+		print 'Loading bronze.crm_cust_info';
 
-;
+		truncate table bronze.crm_cust_info;
 
+		bulk insert bronze.crm_cust_info
+		from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		with 
+			(
+			firstrow = 2
+			, fieldterminator = ','
+			, tablock
+			);
 
+		-- bronze.crm_prd_info -- 
 
+		print 'Loading bronze.crm_prd_info';
 
--- bronze.crm_prd_info -- 
+		truncate table bronze.crm_prd_info;
 
-truncate table bronze.crm_prd_info
+		bulk insert bronze.crm_prd_info
+		from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		with 
+			(
+			firstrow = 2
+			, fieldterminator = ','
+			, tablock
+			);
 
-;
+		-- bronze.crm_sales_details -- 
 
-bulk insert bronze.crm_prd_info
-from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-with 
-	(
-	firstrow = 2
-	, fieldterminator = ','
-	, tablock
-	)
+		print 'Loading bronze.crm_sales_details';
 
-;
-select count(*)
-from bronze.crm_prd_info
+		truncate table bronze.crm_sales_details;
 
-;
-
--- bronze.crm_sales_details -- 
-
-truncate table bronze.crm_sales_details
-
-;
-
-bulk insert bronze.crm_sales_details
-from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-with 
-	(
-	firstrow = 2
-	, fieldterminator = ','
-	, tablock
-	)
-
-;
-select *
-from bronze.crm_sales_details
+		bulk insert bronze.crm_sales_details
+		from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		with 
+			(
+			firstrow = 2
+			, fieldterminator = ','
+			, tablock
+			);
 
 
--- bronze.erp_cust_az12 -- 
+		print 'Loading ERP Table';
 
-truncate table bronze.erp_cust_az12
+		-- bronze.erp_cust_az12 -- 
 
-;
+		print 'Loading bronze.erp_cust_az12';
 
-bulk insert bronze.erp_cust_az12
-from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-with 
-	(
-	firstrow = 2
-	, fieldterminator = ','
-	, tablock
-	)
+		truncate table bronze.erp_cust_az12;
 
-;
-select *
-from bronze.erp_cust_az12
-
--- bronze.erp_loc_a101 -- 
-
-truncate table bronze.erp_loc_a101
-
-;
-
-bulk insert bronze.erp_loc_a101
-from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-with 
-	(
-	firstrow = 2
-	, fieldterminator = ','
-	, tablock
-	)
-
-;
-select *
-from bronze.erp_loc_a101
+		bulk insert bronze.erp_cust_az12
+		from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		with 
+			(
+			firstrow = 2
+			, fieldterminator = ','
+			, tablock
+			);
 
 
--- bronze.erp_pxcat_g1v2 -- 
+		-- bronze.erp_loc_a101 -- 
 
-truncate table bronze.erp_pxcat_g1v2
+		print 'Loading bronze.erp_loc_a101';
 
-;
+		truncate table bronze.erp_loc_a101;
 
-bulk insert bronze.erp_pxcat_g1v2
-from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-with 
-	(
-	firstrow = 2
-	, fieldterminator = ','
-	, tablock
-	)
+		bulk insert bronze.erp_loc_a101
+		from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		with 
+			(
+			firstrow = 2
+			, fieldterminator = ','
+			, tablock
+			);
 
-;
-select *
-from bronze.erp_pxcat_g1v2
 
+
+		-- bronze.erp_pxcat_g1v2 -- 
+
+		print 'Loading bronze.erp_pxcat_g1v2';
+
+		truncate table bronze.erp_pxcat_g1v2;
+
+		bulk insert bronze.erp_pxcat_g1v2
+		from 'C:\Users\qcuon\Desktop\de-starter-pack\Resources\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		with 
+			(
+			firstrow = 2
+			, fieldterminator = ','
+			, tablock
+			);
+	end try
+
+	begin catch
+
+		print 'Error occured';
+		print 'Error message ' + error_message();
+		print 'Error number ' + cast(error_number() as nvarchar) ;
+
+	end catch;
+	
+	set @end_time = getdate();
+
+	print '>> Load duration: ' + cast(datediff(second, @start_time, @end_time) as nvarchar) + ' seconds';
+
+end;
